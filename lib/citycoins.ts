db56901ed09f5c036b44e78fd62e3b7ad4ef127a
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-06-15 14:09:45 -0700

    fix: add waitUntilBlock code
    Also adds getTotalMempoolTx to stacks library.
    
    Also implements some prettier formatting (more semicolons).

--- lib/citycoins.ts
+++ lib/citycoins.ts
@@ -2,7 +2,11 @@ import { fetchJson } from "./utils";
 
 const CC_API_BASE = `https://citycoins-api.citycoins.workers.dev`;
 
-export async function getCCBalance(version: string, city: string, address: string): Promise<string> {
+export async function getCCBalance(
+  version: string,
+  city: string,
+  address: string
+): Promise<string> {
   const url = `${CC_API_BASE}/${version}/${city}/token/get-balance/${address}`;
   const result = await fetchJson(url);
   return result.value;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import throttledQueue from "throttled-queue";
+import { getStacksBlockHeight, getTotalMempoolTx } from "./stacks";
 
 // debug settings for more verbose logging
 const ENABLE_LOGS = false;
@@ -51,7 +52,58 @@ export const fetchJson = async (url: string): Promise<any> => {
 };
 
 // wait for Stacks block height before continuing
-export async function waitUntilBlock(block: number): Promise<boolean> {
+export async function waitUntilBlock(
+  block: number,
+  address: string
+): Promise<boolean> {
+  // initial config
+  var init = true;
+  var currentBlock = 0;
+  // loop until target block is reached
+  do {
+    if (init) {
+      init = false;
+    } else {
+      // pause based on distance to target block
+      if (block - currentBlock > 25) {
+        // over 25 blocks (4 hours / 240 minutes)
+        // check every 2hr
+        await sleep(7200000);
+      } else if (block - currentBlock > 5) {
+        // between 5-25 blocks (50 minutes - 4 hours)
+        // check every 30min
+        await sleep(1800000);
+      } else {
+        // less than 5 blocks (50 minutes)
+        // check every 5min
+        await sleep(300000);
+      }
+    }
+    // print title and info
+    printDivider();
+    console.log(`STATUS: WAITING FOR TARGET BLOCK ${block.toLocaleString()}`);
+    printDivider();
+    printTimeStamp();
+    console.log(`account: ${address.slice(0, 5)}...${address.slice(-5)}`);
+    // get current block
+    currentBlock = await getStacksBlockHeight().catch((err) => exitError(err));
+    console.log(`currentBlock: ${currentBlock.toLocaleString()}`);
+    console.log(`targetBlock: ${block.toLocaleString()}`);
+    // show distance and time
+    if (currentBlock < block) {
+      console.log(
+        `distance: ${(block - currentBlock).toLocaleString()} blocks to go`
+      );
+      const remainingTime = ((block - currentBlock) * 10) / 60;
+      remainingTime >= 1
+        ? console.log(`time: ${remainingTime.toFixed(2)} hours}`)
+        : console.log(`time: ${(remainingTime * 60).toFixed()} minutes`);
+    }
+    // show mempool tx count
+    const mempoolTx = await getTotalMempoolTx().catch((err) => exitError(err));
+    console.log(`mempoolTx: ${mempoolTx.toLocaleString()}`);
+  } while (block > currentBlock);
+
   return true;
 }
 
