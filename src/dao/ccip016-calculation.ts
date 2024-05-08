4c4063555b7ad744c1e4912306a708bc846dc290
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-05-08 13:00:08 -0700

    fix: add script to download all CCD007 tx for analysis

--- src/dao/ccip016-calculation.ts
+++ src/dao/ccip016-calculation.ts
@@ -1,3 +1,67 @@
 // Downloads all transactions
 // and performs analysis for CCIP-016
 // https://github.com/citycoins/governance/pull/16
+
+import { TransactionResults } from "@stacks/stacks-blockchain-api-types";
+
+async function downloadCCD007Transactions() {
+  // set contract info
+  const contractDeployer = "SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH";
+  const contractName = "ccd007-citycoin-stacking";
+  const contractAddress = `${contractDeployer}.${contractName}`;
+  // set API info
+  const apiBase = "https://api.mainnet.hiro.so";
+  const endpoint = `/extended/v2/addresses/${contractAddress}/transactions`;
+  // setup url for fetch
+  const url = new URL(endpoint, apiBase);
+  const limit = 50;
+  let offset = 0;
+  url.searchParams.set("limit", limit.toString());
+  url.searchParams.set("offset", offset.toString());
+
+  // make initial fetch to get the total
+  const response = await fetch(url.toString());
+  // pass through if any errors
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch transactions for ${contractAddress} from Stacks Node API: ${response.status}, ${response.statusText}`
+    );
+  }
+  // parse the response
+  const data: TransactionResults = await response.json();
+  console.log("Total transactions: ", data.total);
+  // setup the array to store all transactions
+  const transactions = data.results;
+  console.log(
+    `Fetched transactions: ${transactions.length} / ${data.total} (${Math.round(
+      transactions.length / data.total
+    )}%)`
+  );
+  // loop through all transactions
+  while (transactions.length < data.total) {
+    await new Promise((r) => setTimeout(r, 500));
+    offset += limit;
+    url.searchParams.set("offset", offset.toString());
+    const response = await fetch(url.toString());
+    if (!response.ok) {
+      console.log("Fetch failed for offset: ", offset);
+      console.log("Retrying after 3 seconds");
+      await new Promise((r) => setTimeout(r, 3000));
+      continue;
+    }
+    // parse the response and store in array
+    const data: TransactionResults = await response.json();
+    transactions.push(...data.results);
+    console.log(
+      `Fetched transactions: ${transactions.length} / ${
+        data.total
+      } (${Math.round(transactions.length / data.total)}%)`
+    );
+  }
+}
+
+async function main() {
+  await downloadCCD007Transactions();
+}
+
+main();
